251fcd58ecf6e81e92174e8c10e8d8d1b2bab66b
yuranranran <liuyr514@gmail.com>
2020-12-28 20:52:53 -0500

    Initial commit

---
+++ app/src/main/java/com/laioffer/tinnews/ui/details/DetailsFragment.java
@@ -0,0 +1,43 @@
+package com.laioffer.tinnews.ui.details;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.FragmentDetailsBinding;
+import com.laioffer.tinnews.model.Article;
+import com.squareup.picasso.Picasso;
+
+public class DetailsFragment extends Fragment {
+
+    public DetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Article article = DetailsFragmentArgs.fromBundle(getArguments()).getArticle();
+        FragmentDetailsBinding binding;
+        binding.detailsTitleTextView.setText(article.title);
+        binding.detailsAuthorTextView.setText(article.author);
+        binding.detailsDateTextView.setText(article.publishedAt);
+        binding.detailsDescriptionTextView.setText(article.description);
+        binding.detailsContentTextView.setText(article.content);
+        Picasso.get().load(article.urlToImage).into(binding.detailsImageView);
+    }
+
+}
